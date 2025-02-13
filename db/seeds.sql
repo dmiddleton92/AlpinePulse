7588e880d476b514cf5790321099d4817cc7e145
dmiddleton92 <dm.middleton92@gmail.com>
2025-02-12 20:11:49 -0700

    updated seeds file adding employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,13 @@ VALUES ('Sales Lead', 100000, 2),
         ('Accountant', 125000, 4),
         ('Legal Team Lead', 250000, 5),
         ('Lawyer', 190000, 5);
+
+INSERT INTO employees (first_name, last_name, role_title, manager_id)
+VALUES ('John', 'Doe', 'Lead Engineer', NULL),
+        ('Mike', 'Chan', 'Sales Lead', NULL),
+        ('Ashley', 'Rodriguez', 'Salesperson', 2),
+        ('Kevin', 'Tupik', 'Lead Engineer', 1),
+        ('Malia', 'Brown', 'Salesperson', 2),
+        ('Sarah', 'Lourd', 'Accountant', 4),
+        ('Tom', 'Allen', 'Legal Team Lead', 6),
+        ('Tina', 'Lee', 'Lawyer', 7);
